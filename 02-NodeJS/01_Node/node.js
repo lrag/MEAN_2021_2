9c94c12b26a5438afef39116ba705e67d9cfa36b
Nombre de usuario <usuario@correo.es>
2021-09-28 11:50:35 +0200

    La asincronia en JS a la luz de las velas

---
+++ 02-NodeJS/01_Node/node.js
@@ -0,0 +1,20 @@
+//
+//En node podremos hacer cosas imposibles en el navegador
+//-acceder al sistema de archivos
+//-comunicarnos con otras aplicaciones (por ejemplo la BB.DD)
+//-enviarle comandos al SO
+//
+//En node.js no disponemos de la mayoría de los objetos implícitos del navegador
+//-document
+//-window
+//-alert
+//-localStorage/sessionStorage
+//-...
+//
+//Algunos si están:
+//-console
+//-JSON
+//-...
+
+//La consola en node es la consola del sistema
+console.log("Hola mundo")
