b15477a989e9edf0edd75a5360b1a4420dc6bac2
Nombre de usuario <usuario@correo.es>
2021-10-05 12:42:58 +0200

    Descubriendo funciones deprecated en el cliente MongoDB

--- 02-NodeJS/05_MongoDB/MongoDB.js
+++ 02-NodeJS/05_MongoDB/MongoDB.js
@@ -1,5 +1,5 @@
 //npm install mongodb
-const mongoDB = require("mongoDB")
+const mongoDB = require("mongodb")
 
 /////////////////////
 // MONGO DB CLIENT //
@@ -9,7 +9,7 @@ const mongoDB = require("mongoDB")
 //-mongoClient.connect
 
 //Funciones síncronas:
-//-
+//-dbs.db("nombre_esquema")
 
 //////////////////////////////////
 //Obtener una conexión a MongoDB//
@@ -23,19 +23,27 @@ const url = "mongodb://localhost:27017"
 
 
 //Creamos el objeto 'MongoClient'
-
-
 const client = new mongoDB.MongoClient(url)
 
-
+//La función connect es asíncrona
+//Nos dan un objeto que representa al servidor de bases de datos
 console.log("Conectando a mongoDB...")
-
 client.connect(function(err, dbs){
     if(err){
         console.log("Error al conectar",err)
         return
     }
     console.log("Conexión establecida")
+
+    //Al objeto dbs le pedimos el esquema que necesitamos
+    let esquemaDiscos = dbs.db("esquema_discos")
+
+    //Al esquema le pedimos la colección que vamos a utilizar
+    let coleccionDiscos = esquemaDiscos.collection("discos")
+
+    //A la colección le pedimos que ejecute consultas
+      
+
 })
 
 console.log("FIN")
