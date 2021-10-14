64498ee75548f8557d49381072d7c696d8318722
Nombre de usuario <usuario@correo.es>
2021-10-14 14:05:10 +0200

    Yo pensaba que JS era mi amigo y me revento la cabeza con un bate de baseball

--- 02-NodeJS/04_REST/recursos/listadoDiscos.js
+++ 02-NodeJS/04_REST/recursos/listadoDiscos.js
@@ -7,9 +7,38 @@ añadir el onclick a los dos botones
 
 listar los discos 
  listarDiscos()
- 
+
 rellenar la tabla
  rellenarTablaDiscos()
 */
 
+function nuevoDisco(){
+    console.log("Nuevo disco")
+}
+
+function listarDiscos(){
+    $.ajax({
+        //url : "http://localhost:3000/discos",
+        url : "discos",
+    })
+    .done(rellenarTablaDiscos)
+    .fail(mostrarError)
+}
+
+function mostrarError(){
+
+}
+
+function rellenarTablaDiscos(){
+    console.log("rellenando la tabla")
+}
+
+$(inicializar)
+
+function inicializar(){
+    $("#btnNuevo").click(nuevoDisco)
+    //$("#btnActualizar").click(???)
+
+    listarDiscos()
+}
 
