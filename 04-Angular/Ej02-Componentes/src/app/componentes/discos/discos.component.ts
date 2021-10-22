f0417395ac6a88f6b6ce0ac1ac94da592fdcc0e3
Nombre de usuario <usuario@correo.es>
2021-10-22 13:52:44 +0200

    Descompuestos con los componentes

--- 04-Angular/Ej02-Componentes/src/app/componentes/discos/discos.component.ts
+++ 04-Angular/Ej02-Componentes/src/app/componentes/discos/discos.component.ts
@@ -10,7 +10,12 @@ export class DiscosComponent implements OnInit {
   public disco:Disco = new Disco()
   public discos:Disco[] = []
 
-  constructor() { }
+  constructor() { 
+    //Pa que haya datos na más empezar
+    this.discos.push( new Disco(1,"1","2",3,"4","5") )
+    this.discos.push( new Disco(2,"11","22",33,"44","55") )
+    this.discos.push( new Disco(3,"111","222",333,"444","555") )
+  }
 
   ngOnInit(): void {
   }
@@ -28,11 +33,49 @@ export class DiscosComponent implements OnInit {
 
   public borrar():void{
     console.log("Borrando...")
+
+    for(let a=0; a<this.discos.length; a++){
+      if( this.discos[a]._id == this.disco._id){
+        this.discos.splice(a,1)
+        break;
+      }
+    }
+    this.vaciar()
   }
 
   public vaciar():void{
-    console.log("Vaciando...")    
+    console.log("Vaciando...")     
     this.disco = new Disco()
+  }
+  
+  public seleccionar(disco:Disco):void{
+    console.log("Seleccionando...")
+  
+    /*
+    for(let d of this.discos){
+      if(d._id == disco._id){
+        //this.disco.titulo = d.titulo
+        //this.disco.grupo = d.grupo
+        //this.disco.year = d.year
+        //this.disco.genero = d.genero
+        //this.disco.notas = d.notas
+        this.disco = d
+      }
+    }
+    */
+
+    let copia:Disco = new Disco(disco._id, disco.titulo, disco.grupo, disco.year, disco.genero, disco.notas)
+
+    this.disco = copia
+
   }
 
 }
+
+
+
+let disco = new Disco(1,"1","2",3,"4","5")
+let copia = disco
+
+copia.titulo = "TDSOTM"
+console.log(disco)
--- 04-Angular/Ej02-Componentes/src/app/entidades/disco.ts
+++ 04-Angular/Ej02-Componentes/src/app/entidades/disco.ts
@@ -3,12 +3,17 @@
 export class Disco {
 
     public constructor(
-        public id    :number|null = null,
+        public _id   :number|null = null,
         public titulo:string|null = null,
         public grupo :string|null = null,
         public year  :number|null = null,
         public genero:string|null = null,
         public notas :string|null = null
-    ){}
+    ){
+        if(_id == null){
+            this._id = Date.now()
+        }
+    }
 
 }
+
