273007e08e0401d81403033f66043bb85e376274
Nombre de usuario <usuario@correo.es>
2021-10-22 10:31:41 +0200

    Descompuestos con los componentes

---
+++ 04-Angular/Ej02-Componentes/src/app/entidades/disco.ts
@@ -0,0 +1,14 @@
+
+
+export class Disco {
+
+    public constructor(
+        public id    :number|null = null,
+        public titulo:string|null = null,
+        public grupo :string|null = null,
+        public year  :number|null = null,
+        public genero:string|null = null,
+        public notas :string|null = null
+    ){}
+
+}
