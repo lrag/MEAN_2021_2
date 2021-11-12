379034007939f87b4e5b84c9abf5620a6aca6b4c
Nombre de usuario <usuario@correo.es>
2021-11-12 09:35:31 +0100

    Siete caballos vienen de bonanza

--- 05-MEAN/01-Usuarios/Angular/src/app/app.module.ts
+++ 05-MEAN/01-Usuarios/Angular/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/cabecera/cabecera.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { PieComponent } from './componentes/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieComponent,
+    MenuComponent,
+    CabeceraComponent
   ],
   imports: [
     BrowserModule
--- 05-MEAN/01-Usuarios/Angular/src/app/componentes/pie/pie.component.ts
+++ 05-MEAN/01-Usuarios/Angular/src/app/componentes/pie/pie.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pie',
-  templateUrl: './pie.component.html',
-  styleUrls: ['./pie.component.css']
+  templateUrl: './pie.component.html'
 })
 export class PieComponent implements OnInit {
 
