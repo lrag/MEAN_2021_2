c448ada53612c309d2fdb8a14dc78b8dda4cf325
Nombre de usuario <usuario@correo.es>
2021-11-26 12:44:24 +0100

    Black friday

--- 05-MEAN/02-Tienda/Angular/src/app/componentes/tienda/producto/producto.component.ts
+++ 05-MEAN/02-Tienda/Angular/src/app/componentes/tienda/producto/producto.component.ts
@@ -11,41 +11,15 @@ import { CestaService } from 'src/app/servicios/cesta-service';
 export class ProductoComponent implements OnInit {
 
   @Input()
-  public producto:Producto|null = null
+  public producto:Producto = new Producto()
   constructor(private cestaService:CestaService) { }
 
   ngOnInit(): void {
   }
 
   public comprar():void{
-
     let cesta:Pedido = this.cestaService.getCesta()
-
-
-
-    let detalle:any = null
-    for(let detalleAux of cesta.detalles){
-      if(detalleAux?.producto?._id == this.producto?._id){
-        detalle = detalleAux
-        break
-      }
-    }
-    if(detalle){
-      detalle.cantidad++
-    } else {
-      detalle = new DetallePedido(this.producto, 1, this.producto?.precio)
-      cesta.detalles?.push(detalle)
-    }
-
-    
-
-
-
-
-    //let detalle:DetallePedido = new DetallePedido(this.producto, 1, this.producto?.precio)
-    //cesta.detalles?.push(detalle)
-    console.log(cesta)    
-
+    cesta.addProducto(this.producto)
   }
 
 }
--- 05-MEAN/02-Tienda/Angular/src/app/entidades/pedido.ts
+++ 05-MEAN/02-Tienda/Angular/src/app/entidades/pedido.ts
@@ -1,4 +1,5 @@
 import { DetallePedido } from "./detallePedido";
+import { Producto } from "./producto";
 import { Usuario } from "./usuario";
 
 export class Pedido {
@@ -13,5 +14,41 @@ export class Pedido {
         public detalles : DetallePedido[] = [],
         public total    : number|null  = null
     ){}
+
+    public addProducto(producto:Producto):void{
+        let detalle:any = null
+        for(let detalleAux of this.detalles){
+          if(detalleAux?.producto?._id == producto._id){
+            detalle = detalleAux
+            break
+          }
+        }
+        if(detalle){
+          detalle.cantidad++
+        } else {
+          detalle = new DetallePedido(producto, 1, producto.precio)
+          this.detalles?.push(detalle)
+        }
+    
+        this.calcularTotal()        
+        console.log(this)         
+    } 
+    
+    public quitarProducto(producto:Producto):void{
+        //
+        //
+        //
+
+        this.calcularTotal
+    } 
+    
+    private calcularTotal():void{
+        let detalle:any
+        let total:number = 0
+        for(detalle of this.detalles){
+          total += detalle.cantidad*detalle.precio
+        }
+        this.total = total
+    }
 
 }
