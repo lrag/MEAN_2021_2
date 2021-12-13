f4c42281fac8bada3b0d9985e55cc0820467c5fb
Nombre de usuario <usuario@correo.es>
2021-12-13 09:36:59 +0100

    Lunes

---
+++ 07_Socket_IO/servidor/servidor.js
@@ -0,0 +1,7 @@
+//npm install socket.io
+const socketIO = require("socket.io")
+const http = require("http")
+
+//Utilizaremos express para servir el cliente (y para otra cosa)
+const express = require("express")
+
