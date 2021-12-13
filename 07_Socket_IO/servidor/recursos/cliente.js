3611acdf11f1ac98c49ef52dcb7e5f29dc6fbb04
Nombre de usuario <usuario@correo.es>
2021-12-13 11:04:10 +0100

    Lunes

--- 07_Socket_IO/servidor/recursos/cliente.js
+++ 07_Socket_IO/servidor/recursos/cliente.js
@@ -1,6 +1,42 @@
 
+function conectar(){
+
+    let alias = $("#alias").val()
+    if(alias.trim().length == 0){
+        alert("Por favor introduzca el alias")
+        return 
+    }
+
+    //La librería nos da el objeto 'io'
+    //La función connect es síncrona
+    //Devuelve un socket, pero que todavía no está establecido puesto
+    //que conectar en realidad es una tarea asíncrona    
+    
+    
+    //
+    //CUIDADO CON ESTE LET, LUIS RAMÓN
+    //    
+    let socket = io.connect(
+        "http://localhost:8000", 
+        {
+            forceNew     : true,
+            reconnection : true
+        } 
+    )
+
+    //Con el evento 'connect' nos enteramos de que la conexión se ha establecido
+    socket.on("connect", conexionEstablecida)
+
+}
+
+function conexionEstablecida(){
+    modoConectado()
+}
+
 function modoDesconectado(){
     $("#btnConectar").prop("disabled",false)
+    $("#alias").prop("disabled",false)
+
     $("#btnDesconectar").prop("disabled",true)
     $("#btnEnviar").prop("disabled",true)
     $("#mensaje").prop("disabled",true)
@@ -8,6 +44,8 @@ function modoDesconectado(){
 
 function modoConectado(){
     $("#btnConectar").prop("disabled",true)
+    $("#alias").prop("disabled",true)
+
     $("#btnDesconectar").prop("disabled",false)
     $("#btnEnviar").prop("disabled",false)
     $("#mensaje").prop("disabled",false)
@@ -15,7 +53,9 @@ function modoConectado(){
 
 $(inicializar)
 function inicializar(){
-    console.log("inicializando")
+    console.log("inicializando...")
+
+    $("#btnConectar").click(conectar)
 
     modoDesconectado()
 }
