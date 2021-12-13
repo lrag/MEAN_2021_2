142c971ce01d41020a8389d0d7a78455eed34c1e
Nombre de usuario <usuario@correo.es>
2021-12-13 09:58:15 +0100

    Lunes

--- 07_Socket_IO/servidor/servidor.js
+++ 07_Socket_IO/servidor/servidor.js
@@ -11,4 +11,29 @@ app.listen(80, function(){
     console.log("Esperando peticiones HTTP en el puerto 80")
 })
 
+//Socket.io no es una implementación de websockets (aunque lo utiliza y se le parece mucho)
+//
+//Necesitamos abrir un puerto para recibir las solicitudes de conexión
+//Para ello utilizamos el módulo 'http'
+//
+//Creamos un objeto server normal y corriente
+let servidor = http.createServer(function(request, response){})
+
+//Una vez que disponemos de un objeto server escuchando en un puerto
+//podemos crear la infraestructura de Socket.io
+const io = socketIO(
+    servidor,
+    {
+        //Configuración para el CORS
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
+//Y arrancamos el servidor
+servidor.listen(8000, function(){
+    console.log("Esperando conexiones Socket.IO en el puerto 8000")
+})
+
+
 
