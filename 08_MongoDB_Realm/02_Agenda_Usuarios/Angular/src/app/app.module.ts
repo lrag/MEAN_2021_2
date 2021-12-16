5df8e5b0aff32e58f95accc6329a2aa25b596406
Nombre de usuario <usuario@correo.es>
2021-12-16 12:14:16 +0100

    No se me ocurre nada

--- 08_MongoDB_Realm/02_Agenda_Usuarios/Angular/src/app/app.module.ts
+++ 08_MongoDB_Realm/02_Agenda_Usuarios/Angular/src/app/app.module.ts
@@ -1,16 +1,69 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './componentes/maquetacion/cabecera/cabecera.component';
+import { MenuComponent } from './componentes/agenda/menu/menu.component';
+import { PieComponent } from './componentes/maquetacion/pie/pie.component';
+import { MaquetacionAgendaComponent } from './componentes/maquetacion/maquetacion-agenda/maquetacion-agenda.component';
+import { MaquetacionLoginComponent } from './componentes/maquetacion/maquetacion-login/maquetacion-login.component';
+import { LoginComponent } from './componentes/usuarios/login/login.component';
+import { RegistroComponent } from './componentes/usuarios/registro/registro.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@Angular/common/http';
+import { PerfilComponent } from './componentes/usuarios/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PieComponent,
+    MenuComponent,
+    LoginComponent,
+    PerfilComponent,
+    CabeceraComponent,
+    RegistroComponent,
+    MaquetacionLoginComponent,
+    MaquetacionAgendaComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule, 
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(AppModule.rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+
+  //Aqui están las rutas para el router-outlet que está en lo alto de la jerarquía
+  static rutas:Routes = [
+    {
+      path      : "",
+      component : MaquetacionLoginComponent,
+      children : [
+        {
+          path      : "",
+          component : LoginComponent
+        },
+        {
+          path      : "registro",
+          component : RegistroComponent
+        }
+      ]
+      
+    },
+    {
+      path      : "agenda",
+      component : MaquetacionAgendaComponent,
+      children  : [
+        {
+          path      : "perfil",
+          component : PerfilComponent
+        }        
+      ]      
+    }
+  ]
+
+}
