93276d5dbb3a978bd0b888fa32d8eeec81a72b15
Nombre de usuario <usuario@correo.es>
2021-12-16 12:21:27 +0100

    No se me ocurre nada

--- 08_MongoDB_Realm/02_Agenda_Usuarios/Angular/src/app/servicios/autenticacion-service.ts
+++ 08_MongoDB_Realm/02_Agenda_Usuarios/Angular/src/app/servicios/autenticacion-service.ts
@@ -8,95 +8,30 @@ import { ConfiguracionUtil } from './configuracion-util';
 @Injectable( { providedIn : "root" } )
 export class AutenticacionService {
 
-    public constructor(private httpClient:HttpClient ){
+    public constructor(){
     }
 
     public getUsuario():Usuario{
-        let json:any = sessionStorage.getItem("usuario")
-        let usuario:Usuario = JSON.parse(json)
-        return usuario
-    }
-
-    public getJWT():string|null{
-        return sessionStorage.getItem("JWT")
+        return new Usuario()
     }
 
     public login(login:string, password:string):Observable<any>{
-
-        return new Observable(subscribers => {            
-            //POST /login
-            //CT:app/json
-            //---------------------------
-            //{ login:AAA, password:BBB }
-            this.httpClient.post(ConfiguracionUtil.urlServidor+"/login", { login:login, password:password })
-                .subscribe(
-                    (respuesta:any) => {
-                        sessionStorage.setItem("JWT",respuesta.jwt)
-                        sessionStorage.setItem("usuario",JSON.stringify(respuesta.usuario))   
-                        subscribers.next()  
-                        subscribers.complete()
-                    },
-                    error => {
-                        console.log(error)
-                        subscribers.error(error)
-                        subscribers.complete()
-                    }
-                )
-        })
-
+        return new Observable()
     }
-
+    
     public logout():void{
-        sessionStorage.removeItem("JWT")
-        sessionStorage.removeItem("usuario")     
     }
-
+    
     public altaUsuario(usuario:Usuario):Observable<any>{
-        return this.httpClient.post(ConfiguracionUtil.urlServidor+"/usuarios", usuario)
+        return new Observable()
     }
-
+    
     public bajaUsuario(usuario:Usuario):Observable<any>{
-
-        return new Observable(subscribers => {
-            this.httpClient.delete(ConfiguracionUtil.urlServidor+"/usuarios/"+usuario._id,
-                                { headers : { Authorization : "Bearer "+this.getJWT() } })
-            .subscribe(
-                respuesta => {
-                    console.log(respuesta)
-                    this.logout()
-                    subscribers.next(respuesta)
-                    subscribers.complete()
-                },
-                err => {
-                    console.log(err)
-                    subscribers.error(err)
-                    subscribers.complete()
-                }
-            )
-        })
-
+        return new Observable()
     }
-
+    
     public modificarUsuario(usuario:Usuario):Observable<any>{
-        return new Observable( subscribers => {
-            this.httpClient.patch(
-                    ConfiguracionUtil.urlServidor+"/usuarios/"+usuario._id,
-                    usuario,
-                    { headers : { Authorization : "Bearer "+this.getJWT() } }
-                )
-                .subscribe(
-                    () => {
-                        sessionStorage.setItem("usuario",JSON.stringify(usuario))
-                        subscribers.next()
-                        subscribers.complete()
-                    },
-                    err => {
-                        console.log(err)
-                        subscribers.error(err)
-                        subscribers.complete()
-                    }
-                )
-        })
+        return new Observable()
     }
 
 }
