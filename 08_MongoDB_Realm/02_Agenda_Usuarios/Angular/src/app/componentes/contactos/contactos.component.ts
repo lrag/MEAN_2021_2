016d568e08b806fec038d8e122d300c11dbb3693
Nombre de usuario <usuario@correo.es>
2021-12-22 13:14:27 +0100

    El baile del async-await

--- 08_MongoDB_Realm/02_Agenda_Usuarios/Angular/src/app/componentes/contactos/contactos.component.ts
+++ 08_MongoDB_Realm/02_Agenda_Usuarios/Angular/src/app/componentes/contactos/contactos.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactosComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ 08_MongoDB_Realm/02_Agenda_Usuarios/Angular/src/app/entidades/contacto.ts
@@ -0,0 +1,13 @@
+
+export class Contacto {
+
+    public constructor(
+        public _id       :string|any = null,
+        public nombre    :string|any = null,
+        public direccion :string|any = null,
+        public telefono  :string|any = null,
+        public correoE   :string|any = null,
+    ){        
+    }
+
+}
